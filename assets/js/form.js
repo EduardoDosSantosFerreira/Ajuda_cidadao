70dda119bdfe9f6562119aaeaed3ea2ad71a5410
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2024-09-25 11:19:56 -0300

    update_6.3.6

--- assets/js/form.js
+++ assets/js/form.js
@@ -12,19 +12,23 @@
         const cpf = document.getElementById('cpf').value;
         const problema = document.getElementById('problema').value;
 
-        // Verifica se os campos estão preenchidos
+        // Verifica se os campos estão preenchidos e se o CPF tem 11 dígitos
         if (nome && cpf && problema) {
-            // Criando a mensagem automática
-            const mensagem = `Olá, meu nome é ${nome}, meu CPF é ${cpf}, e estou com o seguinte problema: ${problema}`;
+            if (cpf.length === 11) {
+                // Criando a mensagem automática
+                const mensagem = `Olá, meu nome é ${nome}, meu CPF é ${cpf}, e estou com o seguinte problema: ${problema}`;
 
-            // Número de telefone fixo
-            const telefone = '13996679977'; // Formato internacional (sem espaços ou caracteres especiais)
+                // Número de telefone fixo
+                const telefone = '5513996679977'; // Formato internacional (sem espaços ou caracteres especiais)
 
-            // URL do WhatsApp com número e mensagem
-            const url = `https://wa.me/55${telefone}?text=${encodeURIComponent(mensagem)}`;
+                // URL do WhatsApp com número e mensagem
+                const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
 
-            // Redirecionar para a URL do WhatsApp, preenchendo automaticamente a mensagem
-            window.open(url, '_blank');
+                // Redirecionar para a URL do WhatsApp, preenchendo automaticamente a mensagem
+                window.open(url, '_blank');
+            } else {
+                alert('Por favor, insira um CPF válido com 11 dígitos!');
+            }
         } else {
             alert('Por favor, preencha todos os campos!');
         }
